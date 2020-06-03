dff324255a45bc0baf5c053e10754ca1f916ec27
denzie <denzie@denzie-Vostro-15-3568>
2020-06-03 23:34:26 +0530

    added suite status

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
 const express = require('express')
 const pug = require('pug');
+//const bodyParser = require("body-parser");
 const rf = require('./readFiles');
 const app = express()
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const path = require('path')
+//const async = require('async')
+const newman = require('newman')
+
 app.use(express.static('public'));
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
 const port = 3000
 app.set('view engine', 'pug');
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/pug', function (req, res) {
+app.get('/suites', function (req, res) {
 	 res.render('index', { 'data': rf.getfiles()});
 });
-app.get('/index', function (req, res) {
-   res.sendFile( __dirname + "/public/" + "index.html" );
-	  var data = rf.getfiles();
-	  res.end(data );
-})
-
-
-app.get("/process_get", function(req,res) {
-	
-	  var data = rf.getfiles();
-	  res.end(data );
+app.get("/status", function(req,res) {
+	var ran = Math.floor(Math.random() * Math.floor(10));
+	var data = {"tot":10, "cur": ran};
+	console.log(data)
+//	res.send(data);
+	res.render('status', data);
 });
 
-app.listen(port, () => console.log(`Example aphttp://marketplace.eclipse.org/marketplace-client-intro?mpc_install=1642532p listening on port ${port}!`))
+app.post("/run", function(req,res) {
+	var data = req.body;
+	newman.run({
+		collection: require('./suites/Demo-1.postman_collection.json'),
+		reporters: 'cli'
+		}, function (err) {
+		if (err) { throw err; }
+		console.log('collection run complete!');
+	});
+	  console.log( data);
+	  var k = "Processing";
+//	  res.end(k+ JSON.stringify(data));
+//	  res.end(JSON.stringify(data));
+	  res.render('status');
+	  
+//	  res.end(" Processing...");
+//	  console.log(data['']);
+//	  var resString = "Running " + data;
+//	  res.end(JSON.stringify(data));
+//	  console.log(data);
+});
+
+app.listen(port, () => console.log(`Suite runner started. listening on port ${port}!`))
--- readFiles.js
+++ readFiles.js
@@ -9,9 +9,7 @@ exports.getfiles = function () {
 	dt=[''];
 	var n='';
 	fs.readdirSync(__dirname).forEach(file => {	
-		console.log(file)
 		if(file.endsWith("json")) {
-			console.log(file)
 			dt.push(file);
 		}
 	});
