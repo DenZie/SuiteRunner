94b4231ed04a2f05340d682363cd27b3cc52734a
Densil Desilva <densil.desilva@ncr.com>
2020-06-04 21:23:02 +0530

    status and progress bar working.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const pug = require('pug');
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const rf = require('./readFiles');
 const app = express()
-
+const fs = require('fs');
+const readline = require('readline');
+const statFile = 'status.txt'
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -12,8 +14,8 @@ const path = require('path')
 const newman = require('newman')
 
 app.use(express.static('public'));
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const port = 3000
 app.set('view engine', 'pug');
@@ -25,23 +27,49 @@ app.get("/status", function(req,res) {
 	res.render('status');
 });
 app.get("/getstat", function(req,res) {
+	var tot='0';
 	var ran = Math.floor(Math.random() * Math.floor(100));
-	var data = {"tot":10, "cur": ran};
+	var lineReader  = readline.createInterface({
+		input: require('fs').createReadStream(statFile),
+	});
+	lineReader.on('line', function (line) {
+		tot = line;
+		lineReader.close();
+	});
+	fileBuffer =  fs.readFileSync(statFile);
+	to_string = fileBuffer.toString();
+	split_lines = to_string.split("\n");
+	var data = {"tot": parseInt(tot), "cur": split_lines.length-1};
 	res.send(data);
 });
 
 app.post("/run", function(req,res) {
-	var data = req.body;
-	console.log(data);
-	console.log("-----------------");
-	newman.run({
-		collection: require('./suites/Demo-1.postman_collection.json'),
-		reporters: 'cli'
-		}, function (err) {
-		if (err) { throw err; }
-		console.log('collection run complete!');
+	var data = Object.values(req.body);
+	var suiteLst= [];
+	
+	fs.writeFile(statFile, data.length , function (err) {
+		  if (err) throw err;
+		});
+	
+	data.forEach(function display(value) { 
+		var suite = { collection: path.join(__dirname, 'suites/' + value  ), reporters: 'html',  reporter : { html : { export : './newman/htmlResults.html'}}};
+		var cmd = function (done) {
+			newman.run(suite).
+			on('start', function (err, args) {
+				console.log("starting Suite :- " + value)
+				fs.appendFile(statFile, "\n"+value , function (err) {
+					  if (err) throw err;
+					});
+			})
+		};
+		suiteLst.push(cmd);
+	} );
+	var i=0;
+	suiteLst.forEach(function display(value) {
+		i=i+1;
+		setTimeout(value, i* 5000);
 	});
-	  res.render('status');
+	  res.render('status', {'tot': suiteLst.length});
 });
 
 app.listen(port, () => console.log(`Suite runner started. listening on port ${port}!`))
